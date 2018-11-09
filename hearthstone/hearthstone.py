6616427a9a61aeb2462b6e81c03fbbe1c473e96a
ef <2425066+drinkfalconpunch@users.noreply.github.com>
2018-11-09 16:23:30 -0600

    more refactoring and starting to work on the card class

--- hearthstone/hearthstone.py
+++ hearthstone/hearthstone.py
@@ -1,17 +1,19 @@
 from .cardback import HearthstoneCardback
 from .card import HearthstoneCard
+from .mixins import APIMixin
 from .authenticator import Authenticator
 
-class HearthstoneAPI(object):
-    API_LOCALES = ('enUS', 'enGB', 'deDE', 'esES', 'esMX', 'frFR', 'itIT',
-                   'koKR', 'plPL', 'ptBR', 'ruRU', 'zhCN', 'zhTW', 'jaJP',
-                   'thTH')
-
-    def __init__(self, api_key=None, locale='enUS', **kwargs):
+class HearthstoneAPI(APIMixin):
+    def __init__(self, api_key=None, locale=None, **kwargs):
         if api_key is None:
             raise AttributeError('API key not found.')
         self.auth = Authenticator(api_key)
+        self._get_locales()
         self._locale = locale
+        self._get_info('patch', self.auth.header)
+
+    def _get_locales(self):
+        setattr(self, 'locales', self._get_info('locales', header=self.auth.header))
 
     @property
     def locale(self):
@@ -19,13 +21,13 @@ class HearthstoneAPI(object):
 
     @locale.setter
     def locale(self, locale):
-        if locale not in self.API_LOCALES:
+        if locale not in self.locales:
             raise ValueError(f'Invalid locale {locale}')
-        self._locale = locale
+        self._locale = locale if locale else 'enUS'
 
     @property
     def cardback(self):
-        return HearthstoneCardback(header=self.auth.header, locale=self._locale)
+        return HearthstoneCardback(self.auth.header, locale=self._locale)
 
     @property
     def card(self):
--- hearthstone/mixins.py
+++ hearthstone/mixins.py
@@ -7,6 +7,14 @@ class APIMixin(object):
     def _get_api_url(self, *assets):
         return slash_join(HEARTHSTONE_URL, *assets)
 
+    def _get_info(self, value=None, header=None):
+        infos = self.get_asset('info', header=header)
+        if value:
+            setattr(self, value, infos[value])
+        else:
+            for value in infos.keys():
+                setattr(self, value, infos[value])
+
     def get_asset(self, *assets, header=None, params=None, callback=None):
         url = self._get_api_url(*assets)
         return requests.get(url, headers=header, params=params).json()
