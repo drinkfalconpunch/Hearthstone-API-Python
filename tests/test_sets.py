5eadb84b26f3b9005540478466bdddf28cd8da90
John Hudson <drinkfalconpunch@me.com>
2018-11-13 15:24:14 -0600

    adding sets tests

--- tests/test_sets.py
+++ tests/test_sets.py
@@ -3,3 +3,13 @@ import pytest
 
 from credentials import api_key
 from hearthstone import HearthstoneAPI
+
+@vcr.use_cassette('tests/cassettes/test_sets.yml')
+def test_get_card_set_is_wild(set_name='classic'):
+    r = HearthstoneAPI(api_key=api_key)
+    assert r.sets.get_card_set(set_name).wild == True
+
+@vcr.use_cassette('tests/cassettes/test_sets.yml')
+def test_get_card_set_is_standard(set_name='classic'):
+    r = HearthstoneAPI(api_key=api_key)
+    assert r.sets.get_card_set(set_name).standard == True
