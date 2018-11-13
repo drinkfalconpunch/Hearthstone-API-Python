2ea1be23da61ca396dcc5b44647ec2a5c8c79598
John Hudson <drinkfalconpunch@me.com>
2018-11-13 16:36:28 -0600

    commented out lots of card stuff to remove

--- hearthstone/card.py
+++ hearthstone/card.py
@@ -1,8 +1,8 @@
 import string
 
 from .base import HearthstoneBase
-from .mixins import APIMixin
-from .const import HEARTHSTONE_URL, CARD_ATTRIBUTES
+# from .mixins import APIMixin
+from .const import CARD_ATTRIBUTES
 
 
 class HearthstoneCard(HearthstoneBase):
@@ -10,50 +10,50 @@ class HearthstoneCard(HearthstoneBase):
         super(HearthstoneCard, self).__init__(CARD_ATTRIBUTES, **attributes)
 
 
-class HearthstoneCardSet(APIMixin):
+# class HearthstoneCardSet(APIMixin):
 
-    def __init__(self, header, locale, **kwargs):
-        self.header = header
-        self.locale = locale
-        self.callback = kwargs.pop('callback', None)
+#     def __init__(self, header, locale, **kwargs):
+#         self.header = header
+#         self.locale = locale
+#         self.callback = kwargs.pop('callback', None)
 
         # factions = self._get_info('factions', self.header)
         # {'attack': None, 'collectible': None, 'cost': None, 'durability': None, 'health': None}
 
-    def _get_cards_by_set(self, card_set, params=None, callback=None):
-        if not isinstance(card_set, str):
-            raise ValueError(f'Card set must be a string. {card_set}')
-        request = self.get_asset(
-            'cards',
-            'sets',
-            string.capwords(card_set),
-            header=self.header,
-            params=params,
-            callback=callback)
-        return request
+    # def _get_cards_by_set(self, card_set, params=None, callback=None):
+    #     if not isinstance(card_set, str):
+    #         raise ValueError(f'Card set must be a string. {card_set}')
+    #     request = self.get_asset(
+    #         'cards',
+    #         'sets',
+    #         string.capwords(card_set),
+    #         header=self.header,
+    #         params=params,
+    #         callback=callback)
+    #     return request
 
-    def _find_cardback(self, cardback_name):
-        if not cardback_name:
-            return self.cardbacks
-        if not isinstance(cardback_name, str):
-            raise ValueError('Cardback name must be a string.')
-        cardback_name = string.capwords(cardback_name)
-        for back in self.cardbacks:
-            if cardback_name == back.name:
-                return back
-        raise ValueError('Cardback name not found.')
+    # def _find_cardback(self, cardback_name):
+    #     if not cardback_name:
+    #         return self.cardbacks
+    #     if not isinstance(cardback_name, str):
+    #         raise ValueError('Cardback name must be a string.')
+    #     cardback_name = string.capwords(cardback_name)
+    #     for back in self.cardbacks:
+    #         if cardback_name == back.name:
+    #             return back
+    #     raise ValueError('Cardback name not found.')
 
-    def _get_cards_by_faction(self, faction, params=None, callback=None):
-        faction = string.capwords(faction)
-        if faction not in self.FACTIONS:
-            raise ValueError(f'Invalid faction. {faction}')
-        return self.get_asset(
-            'cards',
-            'factions',
-            faction,
-            header=self.header,
-            params=params,
-            callback=callback)
+    # def _get_cards_by_faction(self, faction, params=None, callback=None):
+    #     faction = string.capwords(faction)
+    #     if faction not in self.FACTIONS:
+    #         raise ValueError(f'Invalid faction. {faction}')
+    #     return self.get_asset(
+    #         'cards',
+    #         'factions',
+    #         faction,
+    #         header=self.header,
+    #         params=params,
+    #         callback=callback)
 
         # for key, value in card.enumerate():
 
