f21b7a93e2535fe222e1d7c6a0de42e9f90716ce
John Hudson <drinkfalconpunch@me.com>
2018-11-15 13:59:35 -0600

    changed tests to be in classes

--- tests/test_cardback.py
+++ tests/test_cardback.py
@@ -1,42 +1,28 @@
 import vcr
 import pytest
 
-from credentials import api_key
-from hearthstone import HearthstoneAPI
+from . import HearthstoneSetup
 
 
-@vcr.use_cassette('tests/cassettes/test_cardback.yml')
-def test_get_cardback_attributes(cardback_name='classic'):
-    r = HearthstoneAPI(api_key=api_key)
-    assert r.cardback.get_cardback_attributes(
-        cardback_name)['name'] == 'Classic'
+class TestCardbackAPI(HearthstoneSetup):
+    @vcr.use_cassette('tests/cassettes/test_cardback.yml')
+    def test_get_cardback_attributes(self, cardback_name='classic'):
+        assert self.hearthstone.cardback.get_cardback_attributes(cardback_name)['name'] == 'Classic'
 
+    @vcr.use_cassette('tests/cassettes/test_cardback.yml')
+    def test_get_cardback_image(self, cardback_name='classic'):
+        assert self.hearthstone.cardback.get_cardback_image(cardback_name) == 'http://wow.zamimg.com/images/hearthstone/backs/original/Card_Back_Default.png'
 
-@vcr.use_cassette('tests/cassettes/test_cardback.yml')
-def test_get_cardback_image(cardback_name='classic'):
-    r = HearthstoneAPI(api_key=api_key)
-    assert r.cardback.get_cardback_image(
-        cardback_name
-    ) == 'http://wow.zamimg.com/images/hearthstone/backs/original/Card_Back_Default.png'
+    @vcr.use_cassette('tests/cassettes/test_cardback.yml')
+    def test_get_cardback_image_animated(self, cardback_name='classic'):
+        assert self.hearthstone.cardback.get_cardback_image_animated(cardback_name) == 'http://wow.zamimg.com/images/hearthstone/backs/animated/Card_Back_Default.gif'
 
+    @vcr.use_cassette('tests/cassettes/test_cardback.yml')
+    def test_get_cardback_description(self, cardback_name='classic'):
+        assert self.hearthstone.cardback.get_cardback_description(
+            cardback_name) == 'The only card back you\'ll ever need.'
 
-@vcr.use_cassette('tests/cassettes/test_cardback.yml')
-def test_get_cardback_image_animated(cardback_name='classic'):
-    r = HearthstoneAPI(api_key=api_key)
-    assert r.cardback.get_cardback_image_animated(
-        cardback_name
-    ) == 'http://wow.zamimg.com/images/hearthstone/backs/animated/Card_Back_Default.gif'
-
-
-@vcr.use_cassette('tests/cassettes/test_cardback.yml')
-def test_get_cardback_description(cardback_name='classic'):
-    r = HearthstoneAPI(api_key=api_key)
-    assert r.cardback.get_cardback_description(
-        cardback_name) == 'The only card back you\'ll ever need.'
-
-
-@vcr.use_cassette('tests/cassettes/test_cardback.yml')
-def test_get_cardback_none(cardback_name='random'):
-    r = HearthstoneAPI(api_key=api_key)
-    with pytest.raises(ValueError):
-        r.cardback.get_cardback_description(cardback_name)
+    @vcr.use_cassette('tests/cassettes/test_cardback.yml')
+    def test_get_cardback_none(self, cardback_name='random'):
+        with pytest.raises(ValueError):
+            self.hearthstone.cardback.get_cardback_description(cardback_name)
--- tests/test_sets.py
+++ tests/test_sets.py
@@ -1,15 +1,13 @@
 import vcr
 import pytest
 
-from credentials import api_key
-from hearthstone import HearthstoneAPI
+from . import HearthstoneSetup
 
-@vcr.use_cassette('tests/cassettes/test_sets.yml')
-def test_get_card_set_is_wild(set_name='classic'):
-    r = HearthstoneAPI(api_key=api_key)
-    assert r.sets.get_card_set(set_name).wild == True
+class TestSetAPI(HearthstoneSetup):
+    @vcr.use_cassette('tests/cassettes/test_sets.yml')
+    def test_get_card_set_is_wild(self, set_name='classic'):
+        assert self.hearthstone.sets.get_card_set(set_name).wild == True
 
-@vcr.use_cassette('tests/cassettes/test_sets.yml')
-def test_get_card_set_is_standard(set_name='classic'):
-    r = HearthstoneAPI(api_key=api_key)
-    assert r.sets.get_card_set(set_name).standard == True
+    @vcr.use_cassette('tests/cassettes/test_sets.yml')
+    def test_get_card_set_is_standard(self, set_name='classic'):
+        assert self.hearthstone.sets.get_card_set(set_name).standard == True
